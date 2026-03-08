a1d4004f102f229d62c279c815180de2b1d4310a
enpotid <xian101221@gmail.com>
2026-03-08 18:28:19 +0900

    add entry point

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@
 
 use core::panic::PanicInfo;
 
-
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
-}
+} 
