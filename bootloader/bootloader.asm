11f5b0149f1ca3255538240267340b902160e584
enpotid <xian101221@gmail.com>
2026-03-20 18:45:45 +0900

    print hello world

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,9 +1,30 @@
-[ORG 0x00]
+[ORG 0x7C00]
 [BITS 16]
 
 section .text
+    xor ax, ax
+    mov ds, ax
 
-jmp $
+    mov ax, 0xB800
+    mov es, ax
+
+    mov bx, 0
+    mov dx, 12
+
+loop:
+    mov al, [hello + bx]
+    mov [es:bx], al
+    mov al, [hello + bx + 1]
+    mov [es:bx + 1], al
+    add bx, 2
+
+    sub dx, 1
+    cmp dx, 0
+    jne loop
+
+    jmp $
+
+hello db "H", 0xb, "e", 0xb, "l", 0xb, "l", 0xb, "o", 0xb, " ", 0xb, "W", 0xb, "o", 0xb, "r", 0xb, "l", 0xb, "d", 0xb, "!", 0xb
 
 times 510 - ($ - $$) db  0x00
 
