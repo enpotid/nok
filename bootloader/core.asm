89dd2ce23b7c310fe3473c93355346ab4570e2c5
enpotid <xian101221@gmail.com>
2026-03-29 22:24:53 +0900

    enter protected mode

--- bootloader/core.asm
+++ bootloader/core.asm
@@ -3,21 +3,59 @@
 
     mov ax, cs
     mov ds, ax
-
-    mov ax, 0xB800
-    mov es, ax
-    mov [es:0], "a"
-    mov [es:1], 0xb
-
-loop:
-    jmp loop
+    xor ax, ax
+    mov ss, ax
 
     cli
     lgdt[gdtr]
 
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    jmp $ + 2
+    nop
+    nop
+
+    db 0x66
+    db 0x67
+    db 0xEA
+    dd pm_start + 0x10000
+    dw 0x8
+
+[BITS 32]
+pm_start:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+
+    mov ecx, msg + 0x10000
+    mov eax, msg_len
+    call print
+
+    jmp $
+
+print:
+    push edx
+    push ebx
+    xor edx, edx
+    xor ebx, ebx
+    
+print_loop:
+    mov dh, byte [ecx + ebx]
+    mov byte [0xb8000 + ebx * 2], dh
+    mov byte [0xb8000 + ebx * 2 + 1], 0xb
+    add ebx, 1
+    dec eax
+    jnz print_loop
+    pop ebx
+    pop edx
+    ret
+
 gdtr:
     dw gdt_end - gdt_start - 1
-    dd gdt_start
+    dd gdt_start + 0x10000
 
 gdt_start:
     dq 0x0000000000000000
@@ -38,5 +76,11 @@ gdt_start:
     db 0xCF
     db 0x00
 gdt_end:
+
+msg db " _  _  _  _  _  _                                                               "
+    db "| \| |/ _ \| |/ /                                                               "
+    db "| .` | (_) | ' <                                                                "
+    db "|_|\_|\___/|_|\_\                                                               "
+msg_len equ $ - msg
 
 times 5120 - ($ - $$) db 0x00
